d09be11ce1346e522edaf6055317e2a0ea8addbc
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-05-18 06:34:26 +0200

    add fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,26 @@
-const App = () => {
+import { ChangeEvent, useState } from "react";
+
+const App = (): JSX.Element => {
+  const [location, setLocation] = useState<string>("");
+
+  const getSearchOptions = async (value: string) => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
+        process.env.REACT_APP_API_KEY
+      }`
+    );
+  };
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const value: string = e.target.value.trim();
+    setLocation(value);
+
+    if (value === "") return;
+    getSearchOptions(value);
+  };
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
@@ -13,8 +35,9 @@ const App = () => {
           {" "}
           <input
             type="text"
-            value={""}
+            value={location}
             className="px-2 py-1 border-2 border-white rounded-l-md"
+            onChange={handleChangeInput}
           />
           <button className=" rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500 text-zinc-100 px-2 py-1 curosor-pointer">
             search
