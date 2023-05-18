b9f490812c82d656f31f75434b548436190a533c
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-05-18 14:43:51 +0200

    move all JavaScript functions to the own created hook: getForecast()

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,9 @@
-import { ChangeEvent, useEffect, useState } from "react";
-import { optionType } from "./types";
 import Search from "./components/Search";
+import useForecast from "./hooks/useForecast";
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>("");
-  const [city, setCity] = useState<optionType | null>(null);
-  const [options, setOptions] = useState<[]>([]);
-  const [forecast, setForecast] = useState<null>(null);
-
-  const getSearchOptions = async (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()}&limit=5&appid=${
-        process.env.REACT_APP_API_KEY
-      }`
-    )
-      .then((response) => response.json())
-      .then((data) => setOptions(data));
-  };
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value: string = e.target.value.trim();
-    setTerm(value);
-
-    if (value === "") return;
-    getSearchOptions(value);
-  };
-
-  const getForecast = async (city: optionType) => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
-    )
-      .then((response) => response.json())
-      .then((data) => setForecast(data));
-  };
-
-  const onSubmit = () => {
-    if (!city) return;
-
-    getForecast(city);
-  };
-
-  const onOptionSelect = async (option: optionType) => {
-    setCity(option);
-  };
-
-  useEffect(() => {
-    if (city) {
-      setTerm(city.name);
-      setOptions([]);
-    }
-  }, [city]);
-
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast();
   return (
     <main className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[100vh] w-full">
       {forecast ? (
