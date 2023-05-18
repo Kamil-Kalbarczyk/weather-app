250cf408e706d591e6bac40b9e0a8576c457ccba
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-05-18 14:09:20 +0200

    add fetch current weather in chosen location

--- src/App.tsx
+++ src/App.tsx
@@ -23,25 +23,27 @@ const App = (): JSX.Element => {
     getSearchOptions(value);
   };
 
-  const onOptionsSelect = (option: optionType) => {
+  const onOptionsSelect = async (option: optionType) => {
     console.log(option);
     console.log(option.name);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${option.lat}&lon=${option.lon}&units=metric&appid=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
-
-  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
   return (
     <main className="flex justify-center items-center bg-gradient-to-r from-cyan-500 to-blue-500 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-zinc-700">
         <h1 className="text-4xl font-thin">
-          <span className=" font-black">Weather</span> Forecast App
+          <span className=" font-black">Weather</span> App
         </h1>
         <p className=" text-sm mt-2">
           Start to typing name of the place you want to know the weather and
           select the one.
         </p>
         <div className=" flex mt-10 md:mt-4 relative">
-          {" "}
           <input
             type="text"
             value={location}
