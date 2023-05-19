94c7f90f033016078a2c13371afe6e98d4edd05a
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-05-19 12:52:51 +0200

    add Degree component

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -4,7 +4,14 @@ type Props = {
   data: forecastType;
 };
 
+const Degree = ({ temp }: { temp: number }): JSX.Element => (
+  <span>
+    {temp} <sup>o</sup>C
+  </span>
+);
+
 const Forecast = ({ data }: Props): JSX.Element => {
+  const today = data.list[0];
   return (
     <div className="w-full md:max-w-[500px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg">
       <div className="mx-auto w-[300px]">
@@ -12,6 +19,9 @@ const Forecast = ({ data }: Props): JSX.Element => {
           <h2 className=" text-2xl font-black">
             {data.name} <span className=" font-thin">{data.country}</span>
           </h2>
+          <h1 className=" text-4xl font-extralight">
+            <Degree temp={Math.round(today.main.temp)} />
+          </h1>
         </section>
       </div>
     </div>
