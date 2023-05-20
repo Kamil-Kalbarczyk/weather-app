9593fb0aa048caead1a4bd57a4e77d0fe6958fdb
Kamil Kalbarczyk <k.kalbarczyk1@gmail.com>
2023-05-20 06:41:44 +0200

    add feel temperature

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -72,6 +72,16 @@ const Forecast = ({ data }: Props): JSX.Element => {
               Math.round(today.wind.deg)
             )} gust ${today.wind.gust.toFixed(1)} km/h`}
           />
+          <Tile
+            icon="feels"
+            title="Feel"
+            info={<Degree temp={Math.round(today.main.feels_like)} />}
+            description={`Feels ${
+              Math.round(today.main.feels_like) < Math.round(today.main.temp)
+                ? "colder"
+                : "warmer"
+            }`}
+          />
         </section>
       </div>
     </div>
